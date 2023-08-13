b688591854d84372bcc1cd18deb0e84014ac619f
Angry Coding <mailidanielifechukwu@gmail.com>
2023-08-13 03:50:10 +0100

    changed dashboard path

---
+++ packages/nextjs/pages/dashboard/transactions.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageHOC from "~~/components/superhack/PageHOC";
+
+const Transactions = () => {
+  return <div>Transactions</div>;
+};
+
+export default PageHOC(Transactions);
---
+++ packages/nextjs/pages/dashboard/transfer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageHOC from "~~/components/superhack/PageHOC";
+
+const Transfer = () => {
+  return <div>Transfer</div>;
+};
+
+export default PageHOC(Transfer);
