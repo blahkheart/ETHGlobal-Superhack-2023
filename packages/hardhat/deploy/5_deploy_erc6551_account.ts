4fb97a779dcd6af01c91c8483643292fdb8e00c3
blahkheart <dannithomx@gmail.com>
2023-08-14 00:49:35 +0100

    pulled latest changes from origin

---
+++ packages/hardhat/deploy/5_deploy_erc6551_account.ts
@@ -0,0 +1,27 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+
+const deployAccountRegistry: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+  
+  await deploy("ERC6551Account", {
+    from: deployer,
+    // Contract constructor arguments
+    args: [],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+
+  // Get the deployed contract
+  // const yourContract = await hre.ethers.getContract("YourContract", deployer);
+};
+
+export default deployAccountRegistry;
+
+// Tags are useful if you have multiple deploy files and only want to run one of them.
+// e.g. yarn deploy --tags YourContract
+deployAccountRegistry.tags = ["ERC6551Account"];
