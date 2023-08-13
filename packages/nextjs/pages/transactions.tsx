cfcd4db1f3e69d41c44094be4c32d7f29ddcf318
Angry Coding <mailidanielifechukwu@gmail.com>
2023-08-13 03:48:50 +0100

    removed files

--- packages/nextjs/pages/transactions.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PageHOC from "~~/components/superhack/PageHOC";
-
-const Transactions = () => {
-  return <div>Transactions</div>;
-};
-
-export default PageHOC(Transactions);
--- packages/nextjs/pages/transfer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PageHOC from "~~/components/superhack/PageHOC";
-
-const Transfer = () => {
-  return <div>Transfer</div>;
-};
-
-export default PageHOC(Transfer);
